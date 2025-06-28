b787cf65a714ccdc265622274a8544d772fe247b
firatyedibela <yedibelafirat0@gmail.com>
2025-06-28 15:59:30 +0300

    refactor: use message instead of value

--- src/utils/validations.tsx
+++ src/utils/validations.tsx
@@ -1,43 +1,25 @@
 const validations = {
   firstName: {
-    required: {
-      value: true,
-      message: 'This field is required',
-    },
+    required: 'This field is required',
   },
   lastName: {
-    required: {
-      value: true,
-      message: 'This field is required',
-    },
+    required: 'This field is required',
   },
   email: {
-    required: {
-      value: true,
-      message: 'This field is required',
-    },
+    required: 'This field is required',
     pattern: {
       value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
       message: 'Please enter a valid email address',
     },
   },
   queryType: {
-    required: {
-      value: true,
-      message: 'Please select a query type',
-    },
+    required: 'Please select a query type',
   },
   message: {
-    required: {
-      value: true,
-      message: 'This field is required',
-    },
+    required: 'This field is required',
   },
   consentToContact: {
-    required: {
-      value: true,
-      message: 'To submit this form, please consent to being contacted',
-    },
+    required: 'To submit this form, please consent to being contacted',
   },
 };
 
